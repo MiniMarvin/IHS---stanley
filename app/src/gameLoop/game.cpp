b14883915b9d690cc61d3f94e60fe5f7bc10add5
izabella <cmeloizabella@gmail.com>
2022-05-01 21:41:51 +0000

    fix green leds

--- app/src/gameLoop/game.cpp
+++ app/src/gameLoop/game.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <bitset>
 #include <vector>
+#include <tuple>
 
 #include "../de2iInterface/de2iInterface.hpp"
 
@@ -32,7 +33,7 @@ vector<int> getOrderOfGreenLeds(int count) {
     vector<int> orderToLightUp;
     
     for(int i = 0; i < count; i++) {
-        int value = rand()%8; // 8 positions (0 to 7)
+        int value = rand()%4; // 4 positions (0 to 3)
         orderToLightUp.push_back(value);
     }
 
@@ -41,19 +42,32 @@ vector<int> getOrderOfGreenLeds(int count) {
     return orderToLightUp;
 }
 
-void lightUpAndLightDownGreenLed(int position, De2iInterface interface) {
-    cout << "Acendendo LED Verde na posição " << position << endl;
-    interface.writeGreenLed(0, position);
-    
+// 7 6 5 4 3 2 1 0 --- índice leds
+// l l l l l l l l --- leds
+// ( ) ( ) ( ) ( ) --- botões
+//  3   2   1   0  --- índice botões
+pair<int, int> translateButtonToGreenLed(int position) {
+    int firstLed = position*2;
+    int secondLed = firstLed + 1;
+    return make_pair(firstLed, secondLed);
+}
+
+void lightUpAndLightDownGreenLed(pair<int, int> positions, De2iInterface interface) {
+    cout << "Acendendo LED Verde na posição " << positions.first  << " e " << positions.second << endl;
+    interface.writeGreenLed(0, positions.first);
+    interface.writeGreenLed(0, positions.second);
+
     sleep(1);
     
-    cout << "Apagando LED Verde na posição " << position << endl;
-    interface.writeGreenLed(1, position);
+    cout << "Apagando LED Verde na posição " << positions.first  << " e " << positions.second << endl;
+    interface.writeGreenLed(1, positions.first);
+    interface.writeGreenLed(1, positions.second);
 }
 
 void lightUpGreenLightFromVector(vector<int> array, De2iInterface interface) {
     for(int i = 0; i < array.size(); i++){
-        lightUpAndLightDownGreenLed(array[i], interface);
+        pair<int, int> ledsToLightUp = translateButtonToGreenLed(array[i]);
+        lightUpAndLightDownGreenLed(ledsToLightUp, interface);
     }
 }
 
@@ -78,12 +92,12 @@ int findPosition(unsigned n) {
 
 // Como são 8 leds e 4 botões, vamos considerar que as leds 0 e 1 são equivalentes ao botão 0, 
 // a led 2 e 3 são equivalentes ao botão 2 e assim por diante
-bool checkIfPositionOfButtonIsEquivalentOfGreenLight(int positionOfButtonClicked, int positionOfGreenLight) {
-    cout << "Posição do botão: " << positionOfButtonClicked << endl;
-    cout << "Posição da LED: " << positionOfGreenLight << endl;
-    if(positionOfButtonClicked == positionOfGreenLight || positionOfButtonClicked == positionOfGreenLight - 1) {
+bool checkIfPositionOfButtonIsEquivalentOfGreenLight(pair<int, int> positionsOsLedsTranslated, int positionOfGreenLight) {
+    cout << "Posição dos LEDs referentes ao botão clicado: " << positionsOsLedsTranslated.first << " " << positionsOsLedsTranslated.second << endl;
+    cout << "Posição da LED piscado: " << positionOfGreenLight << endl;
+    
+    if(positionsOsLedsTranslated.first == positionOfGreenLight || positionsOsLedsTranslated.second == positionOfGreenLight)
         return true;
-    }
     
     return false;
 }
@@ -97,9 +111,59 @@ void updatePoints(De2iInterface interface) {
     TOTAL_POINTS++;
     showPoints(interface);
 }
+
 void resetPoints(De2iInterface interface) {
     TOTAL_POINTS = 0;
     showPoints(interface);
+}
+
+bool runGreenLedsAndPushButtonsGameAndCheckIfWin(int numberOfLeds, De2iInterface interface) {
+    vector<int> orderToLightUp = getOrderOfGreenLeds(numberOfLeds);
+    lightUpGreenLightFromVector(orderToLightUp, interface);
+    
+    cout << "Esperando botões..." << endl;
+    int LED_INDEX = 0;
+    // Loop para esperar interações do usuário
+    while(1) {
+        unsigned int buttons = interface.readButtons();
+        cout << "Valor lido: " << buttons << endl;
+        
+        // Pega a posição do botão apertado; retorna -1 se nenhum ou mais de um forem apertados ao mesmo tempo.
+        int positionOfButtonClicked = findPosition(buttons);
+        cout << "Posição do botão clicado: " << positionOfButtonClicked << endl;
+    
+        pair<int, int> ledsToLightUp = translateButtonToGreenLed(positionOfButtonClicked);
+        
+        // Se a posição for igual a esperada
+        if(checkIfPositionOfButtonIsEquivalentOfGreenLight(ledsToLightUp, orderToLightUp[LED_INDEX])) {
+            lightUpAndLightDownGreenLed(ledsToLightUp, interface);
+            
+            // Se a posição for a última, vai para a próxima fase
+            if(LED_INDEX == orderToLightUp.size() - 1) {
+                cout << "Parabéns! Seguindo para o segundo nível!" << endl;
+                return true;
+            }
+            
+            LED_INDEX++;
+            updatePoints(interface);
+            
+        } else {
+            // Se apertar um botão errado, acende todos os leds e retorna para a primeira fase
+            interface.writeGreenLeds(0b11111111);
+            interface.writeGreenLeds(0b11111111);
+            
+            sleep(1);
+            cout << "Errou :( Voltando para o primeiro nível" << endl;
+            
+            return false;
+        }
+        
+        sleep(1);
+    }
+}
+
+bool runRedLedsAndSwitchesAndCheckIfWin(De2iInterface interface) {
+    
 }
 
 int gameLoop(char* driverPath) {
@@ -111,47 +175,12 @@ int gameLoop(char* driverPath) {
     // Primeira fase: acende aleatoriamente cinco valores de leds verde e compara com os botões apertados
     first_level:
         resetPoints(interface);
-
-        int QTD_LEDS = 5;
-        vector<int> orderToLightUp = getOrderOfGreenLeds(QTD_LEDS);
-        lightUpGreenLightFromVector(orderToLightUp, interface);
+        bool win = runGreenLedsAndPushButtonsGameAndCheckIfWin(5, interface);
+       
+        if(win) goto second_level;
+        goto first_level;
         
-        cout << "Esperando botões..." << endl;
-        int LED_INDEX = 0;
-        while(1) {
-            unsigned int buttons = interface.readButtons();
-            cout << "Valor lido: " << buttons << endl;
-            
-            // Pega a posição do botão apertado; retorna -1 se nenhum ou mais de um forem apertados ao mesmo tempo.
-            int positionOfButtonClicked = findPosition(buttons);
-
-            // Se a posição for igual a esperada
-            if(checkIfPositionOfButtonIsEquivalentOfGreenLight(positionOfButtonClicked, orderToLightUp[LED_INDEX])) {
-                lightUpAndLightDownGreenLed(orderToLightUp[LED_INDEX], interface);
-                
-                // Se a posição for a última, vai para a próxima fase
-                if(LED_INDEX == orderToLightUp.size() - 1) {
-                    cout << "Parabéns! Seguindo para o segundo nível!" << endl;
-                    goto second_level;
-                }
-                
-                LED_INDEX++;
-                updatePoints(interface);
-                
-            } else {
-                // Se apertar um botão errado, acende todos os botões e retorna para a primeira fase
-                interface.writeGreenLeds(0b11111111);
-                
-                sleep(1);
-                cout << "Erroou :( Voltando para o primeiro nível" << endl;
-                
-                resetPoints(interface);
-                goto first_level;
-            }
-            
-            usleep(100000);
-        }
-        
+    // Segunda fase: 
     second_level:
 
     
