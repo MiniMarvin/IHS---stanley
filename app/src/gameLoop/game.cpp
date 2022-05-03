eec34fd9bb81de6c601186d69f2a57733eecf3b9
Caio Gomes <caiogomes34@gmail.com>
2022-05-03 18:05:11 +0000

    fix weird stuff

--- app/src/gameLoop/game.cpp
+++ app/src/gameLoop/game.cpp
@@ -88,7 +88,7 @@ void updatePeriferals(PerifericValues& periferics, De2iInterface interface, Time
     interface.leftDisplayWrite(periferics.getDisplayLeft());
     interface.writeGreenLeds(periferics.getGreenLeds());
     interface.writeRedLeds(periferics.getRedLeds());
-    cout << "Writing leds: " << bitset<4>(periferics.getGreenLeds()) << endl;
+    cout << "Writing leds: " << bitset<8>(periferics.getGreenLeds()) << endl;
 }
 
 // ===========================================================
